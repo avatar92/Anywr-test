6d7c329c6142c60501c4288c1c8bfecc26d345b9
adel <adelbensalem1992@gmail.com>
2023-05-21 00:52:56 +0200

    chore: intialize server project
    add basic express server project
    add configuration files (server/.env, server/.gitignore, server/package.json)
    add required dependicies (bcrypt, body-parser, dotenv, express, express-validator, jsonwebtoken, mongoose)
    intialing data base connection

---
+++ server/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+
+app.use(bodyParser.json());
+app.use((req,res,next)=>{
+    
+    res.header('Access-Control-Allow-Origin',process.env.APP_ORIGIN);
+    res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers','content-type, Authorisation');
+    next();
+});
+
+
+mongoose.connect(process.env.CONNECTION_STRING).then((res)=>{
+    app.listen(process.env.PORT,()=>{
+        console.log('server is running in pot ',process.env.PORT);
+    });
+}).catch(err=>{
+    console.log(err);
+})
+
