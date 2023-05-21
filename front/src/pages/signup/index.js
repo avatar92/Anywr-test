80ad91a4ed455e87c486a8aea3db215e5eb38475
adel <adelbensalem1992@gmail.com>
2023-05-21 19:15:39 +0200

    feat(signup): add sign up form

--- front/src/pages/signup/index.js
+++ front/src/pages/signup/index.js
@@ -1,9 +1,55 @@
-
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 const SignUp = () => {
-    return <div>
-        Welcome To Signup page
-    </div>
+    const handleOnsubmit = e => {
+        e.preventDefault();
+        
+    }
+    return <div className="container my-5">
+    <Form onSubmit={handleOnsubmit}>
+        <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="2">
+                UserName
+            </Form.Label>
+            <Col sm="10">
+                <Form.Control type='text' placeholder='your username' />
+            </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="2">
+                Email
+            </Form.Label>
+            <Col sm="10">
+                <Form.Control type='email' placeholder='email@example.com' />
+            </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="2">
+                Password
+            </Form.Label>
+            <Col sm="10">
+                <Form.Control type="password" placeholder="Password" />
+            </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="2">
+                Confirm password
+            </Form.Label>
+            <Col sm="10">
+                <Form.Control type="password" placeholder="Confirm password" />
+            </Col>
+        </Form.Group>
+        <div className='d-flex justify-content-end'>
+            <Button type="submit" variant="secondary" size="lg">
+                Sign in
+            </Button>
+        </div>
+
+    </Form>
+</div>
 }
 
 
