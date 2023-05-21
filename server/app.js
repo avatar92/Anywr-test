9bf9c3aecccb4063bc936ee4732c9eea42f67d1b
adel <adelbensalem1992@gmail.com>
2023-05-21 21:20:38 +0200

    fix(app.js): fix cors import

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require(cors);
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
 const dashRoutes = require('./routes/dash');
