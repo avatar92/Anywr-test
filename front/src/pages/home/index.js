a6b050b35b526a2a9da473c5a8e7ed043f5a9293
adel <adelbensalem1992@gmail.com>
2023-05-21 21:22:18 +0200

    feat(Form validation login): add form validation
    add form validation when user enter empty field
    add form validation when the response of the server is 404

--- front/src/pages/home/index.js
+++ front/src/pages/home/index.js
@@ -1,35 +1,107 @@
+import { useRef, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+import AnwyrTest from '../../services/utils/axios';
+import isEmpty from '../../services/utils/isEmpty';
 
 const Home = () => {
-    const handleOnsubmit = e => {
-        e.preventDefault();
-        
+    const usernameRef = useRef(null);
+    const passwordRef = useRef(null);
+    const [validated, setValidated] = useState(false);
+    const [error, setError] = useState({
+        username: null,
+        password: null,
+    })
+    const [loading, setLoading] = useState(false);
+
+    const handleOnsubmit = event => {
+        const form = event.currentTarget;
+        const { username, password } = validateForm();
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            return;
+        }
+        event.preventDefault();
+        AnwyrTest.post('/auth/login', {
+            username: username,
+            password: password,
+        }).then(res => {
+            const { data } = res;
+            console.log(data);
+        }).catch(err => {            
+            if (err.response) {
+                const {data,status,headers} = err.response;
+                const resErr = {
+                    username: null,
+                    password: null,
+                }
+                if(status === 404){
+                    resErr.username = "Wrong credentials";
+                    resErr.password = "Wrong credentials";
+                }
+                setError(resErr);
+            }
+        })
+    }
+
+    const validateForm = () => {
+        const error = {
+            username: null,
+            password: null
+        };
+        setError(error);
+        if (isEmpty(usernameRef.current.value)) {
+            error.username = "Username is Empty";
+        }
+
+        if (isEmpty(passwordRef.current.value)) {
+            error.password = "Password is Empty";
+        }
+
+        setError(error);
+        return {
+            username: usernameRef.current.value,
+            password: passwordRef.current.value
+        }
     }
     return <div className="container my-5">
-        <Form onSubmit={handleOnsubmit}>
-            <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form noValidate validated={validated} onSubmit={handleOnsubmit}>
+            <Form.Group as={Row} className="mb-3" controlId='validationUsername'>
                 <Form.Label column sm="2">
                     UserName
                 </Form.Label>
                 <Col sm="10">
-                    <Form.Control type='text' placeholder='your username' />
+                    <InputGroup hasValidation>
+                        <Form.Control required isInvalid={!isEmpty(error.username) ? true : false} ref={usernameRef} type='text' placeholder='your username' />
+                        <Form.Control.Feedback type="invalid">
+                            {error.username}
+                        </Form.Control.Feedback>
+                    </InputGroup>
                 </Col>
+
             </Form.Group>
 
-            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+            <Form.Group as={Row} className="mb-3" controlId='validationPassword'>
                 <Form.Label column sm="2">
                     Password
                 </Form.Label>
                 <Col sm="10">
-                    <Form.Control type="password" placeholder="Password" />
+                    <InputGroup hasValidation>
+                        <Form.Control required isInvalid={!isEmpty(error.password) ? true : false} ref={passwordRef} type="password" placeholder="Password" />
+                        <Form.Control.Feedback type="invalid">
+                            {error.password}
+                        </Form.Control.Feedback>
+                    </InputGroup>
                 </Col>
             </Form.Group>
             <div className='d-flex justify-content-end'>
                 <Button type="submit" variant="secondary" size="lg">
-                    Log in
+                    {loading ? <>Loading...</> : <>Log in</>}
                 </Button>
             </div>
 
