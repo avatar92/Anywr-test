255863571ee0a1f7531102350571f34975a7d482
adel <adelbensalem1992@gmail.com>
2023-05-21 22:07:36 +0200

    feat(profile): add user email and username in profile
    add get requet
    render data when they are availalble

--- front/src/pages/profile/index.js
+++ front/src/pages/profile/index.js
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+
+import AnwyrTest from "../../services/utils/axios";
+import { setUser } from '../../services/reducers/user/actions';
+import { connect } from "react-redux";
 
 
-const Profile = () => {
-    return <div>
-        Welcome To Profile page
+const Profile = ({ setUser }) => {
+    const [loading, setLoading] = useState(true);
+    const [currentUser, setCurrentUser] = useState(null);
+    useEffect(() => {
+        AnwyrTest.get('/dash/info').then(res => {
+            setCurrentUser(res.data);
+            setLoading(false);
+        }).catch(err => {
+            if (err.response) {
+                const { data, status, headers } = err.response;
+                if (status === 401) {
+                    setUser({})
+                }
+                setLoading(false);
+            }
+        })
+    }, [])
+    return <div className="container">
+        {loading ? <h1>Loading...</h1> : <>
+            <p>
+                email : {currentUser.email}
+            </p>
+            <p>
+                username : {currentUser.username}
+            </p>
+        </>}
+
     </div>
 }
 
+Profile.propTypes = {
+    setUser: PropTypes.func.isRequired,
+};
 
-export default Profile;
+export default connect(null, { setUser })(Profile);
