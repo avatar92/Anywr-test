3910c454a79561bff59c913ac599ccca099820c4
adel <adelbensalem1992@gmail.com>
2023-05-21 19:07:27 +0200

    feat(Login Form): add login Form

--- front/src/pages/home/index.js
+++ front/src/pages/home/index.js
@@ -1,9 +1,39 @@
-
-
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
-    return <div className="container">
-        This is Home Page
+    const handleOnsubmit = e => {
+        e.preventDefault();
+        
+    }
+    return <div className="container my-5">
+        <Form onSubmit={handleOnsubmit}>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                <Form.Label column sm="2">
+                    UserName
+                </Form.Label>
+                <Col sm="10">
+                    <Form.Control type='text' placeholder='your username' />
+                </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Label column sm="2">
+                    Password
+                </Form.Label>
+                <Col sm="10">
+                    <Form.Control type="password" placeholder="Password" />
+                </Col>
+            </Form.Group>
+            <div className='d-flex justify-content-end'>
+                <Button type="submit" variant="secondary" size="lg">
+                    Log in
+                </Button>
+            </div>
+
+        </Form>
     </div>
 }
 
