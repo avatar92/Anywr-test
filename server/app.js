89026616443ba1b9b52df117023ea4464705f57a
adel <adelbensalem1992@gmail.com>
2023-05-21 20:46:22 +0200

    fix(cors): fix cors middleware
    replace cors middleware with the package cors
    update .env

--- server/app.js
+++ server/app.js
@@ -2,25 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require(cors);
 
 const authRoutes = require('./routes/auth');
 const dashRoutes = require('./routes/dash');
 
 const app = express();
 
-
+const corsOptions = {
+    origin: process.env.APP_ORIGIN.split(','),
+}
 
 app.use(bodyParser.json());
-app.use((req,res,next)=>{
-    
-    res.header('Access-Control-Allow-Origin',process.env.APP_ORIGIN);
-    res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers','content-type, Authorisation');
-    next();
-});
+app.use(cors(corsOptions));
 
-app.use('/auth',authRoutes);
-app.use('/dash',dashRoutes);
+app.use('/auth', authRoutes);
+app.use('/dash', dashRoutes);
 
 app.use((error, req, res, next) => {
     const status = error.statusCode || 500;
@@ -30,11 +27,11 @@ app.use((error, req, res, next) => {
 });
 
 
-mongoose.connect(process.env.CONNECTION_STRING).then((res)=>{
-    app.listen(process.env.PORT,()=>{
-        console.log('server is running in pot ',process.env.PORT);
+mongoose.connect(process.env.CONNECTION_STRING).then((res) => {
+    app.listen(process.env.PORT, () => {
+        console.log('server is running in pot ', process.env.PORT);
     });
-}).catch(err=>{
+}).catch(err => {
     console.log(err);
 })
 
