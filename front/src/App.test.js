ce5c1e889fe86e187a1fd86f6801e25b246a0927
adel <adelbensalem1992@gmail.com>
2023-05-22 00:27:38 +0200

    test: intializing tests

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+const sum = (a, b) => a + b;
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('sums number', () => {
+    expect(sum(1, 2)).toEqual(3);
+    expect(sum(2, 2)).toEqual(4);
+})
